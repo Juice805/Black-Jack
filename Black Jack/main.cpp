68eadadd0b8f6728e86053f1a142843dd0671732
Justin Oroz <justin.oroz@me.com>
2014-11-20 12:05:05 -0800

    And we begin
    Black Jack assignment has begun

---
+++ Black Jack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Black Jack
+//
+//  Created by Justin Oroz on 11/20/14.
+//  Copyright (c) 2014 Justin Oroz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
